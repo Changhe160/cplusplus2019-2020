4dbe68357a64bf3a95e2241a8a1cedaa9496c086
Changhe Li <changhe.lw@gmail.com>
2019-09-01 16:39:21 +0800

    update

--- code/chapter6.2/chapter6.2.cpp
+++ code/chapter6.2/chapter6.2.cpp
@@ -27,27 +27,28 @@ public:
 int main() {
 	//6.2.1
 	{
-		Fraction a;			//默认构造函数
+	/*	Fraction a;			//默认构造函数
 		Fraction b(1, 5);	//直接初始化
 		int i;
 		Foo f(i);
 		
-		Employee member("Kevin"); 
+		Employee member("Kevin"); */
 	}
 	//6.2.2
-	{
+	/*{
 		Fraction a(1, 5);	//直接初始化
 		Fraction b(a);		//直接初始化，b为对象a的拷贝
 		Fraction c = Fraction(3, 2); //复制初始化
-		Fraction d = 7;		//复制初始化
+		//Fraction d = 7;		//复制初始化
 
 		//Fraction d2 = static_cast<Fraction>(7); //当我们将 Fraction 类的默认构造函数声明为 explicit 之后
 
 		Fraction e = divide(b, c);
 
-		Employee e1;
+		//Employee e1;
 		//Employee e2(e1);//错误：不能调用删除的复制构造函数
-	}
+
+	}*/
 	{
 		class Point {
 			double m_x = 0, m_y = 0;
